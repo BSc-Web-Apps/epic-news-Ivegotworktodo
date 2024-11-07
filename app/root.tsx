ef6e09e008f9e906dd0ac0b9bfd2bf886d39123e
Joshua Blake <115797116+Ivegotworktodo@users.noreply.github.com>
2024-11-07 16:30:25 +0000

    a

--- app/root.tsx
+++ app/root.tsx
@@ -16,8 +16,14 @@ export default function App() {
 		<Document nonce={nonce}>
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
-					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">Welcome to Epic News!</h1>
+					<main className="grid h-full place-items-center bg-blue-400">
+						<h1 className="px-60 text-center text-mega text-red-400">
+							¡ǝɯoɔlǝM
+						</h1>
+						<p className="text-5xl text-yellow-500">The cat ate the bird</p>
+						<button className="transition=all h-1/2 w-1/3 rounded bg-blue-500 px-4 py-2 font-bold text-white duration-500 hover:bg-red-700 hover:text-6xl md:text-lg lg:text-xl">
+							Help Me!
+						</button>
 					</main>
 				</div>
 			</div>
