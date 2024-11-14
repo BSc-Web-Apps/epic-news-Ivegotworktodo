ed091d586da66b8d98f1db86ad9abc7e368e8eee
Joshua Blake <115797116+Ivegotworktodo@users.noreply.github.com>
2024-11-14 14:04:25 +0000

    Added 2 buttons

--- app/root.tsx
+++ app/root.tsx
@@ -16,14 +16,27 @@ export default function App() {
 		<Document nonce={nonce}>
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
-					<main className="grid h-full place-items-center bg-blue-400">
+					<main className="container grid h-full place-items-center bg-orange-200">
 						<h1 className="px-60 text-center text-mega text-red-400">
 							¡ǝɯoɔlǝM
 						</h1>
 						<p className="text-5xl text-yellow-500">The cat ate the bird</p>
-						<button className="transition=all h-1/2 w-1/3 rounded bg-blue-500 px-4 py-2 font-bold text-white duration-500 hover:bg-red-700 hover:text-6xl md:text-lg lg:text-xl">
-							Help Me!
-						</button>
+						<div className="flex gap-8">
+							<button className="transition=all h-1/2 w-1/3 rounded bg-blue-500 px-4 py-2 font-bold text-white duration-500 hover:bg-red-700 hover:text-6xl md:text-lg lg:text-xl">
+								Goodbye!
+							</button>
+							<button className="transition=all h-1/2 w-1/3 rounded bg-blue-500 px-4 py-2 font-bold text-white duration-500 hover:bg-red-700 hover:text-6xl md:text-lg lg:text-xl">
+								Goodbye!
+							</button>
+							<button className="transition=all h-1/2 w-1/3 rounded bg-blue-500 px-4 py-2 font-bold text-white duration-500 hover:bg-red-700 hover:text-6xl md:text-lg lg:text-xl">
+								Goodbye!
+							</button>
+						</div>
+						<div className="w-1/2">
+							<section className="relative h-1/6 border-8 border-yellow-500"></section>
+							<section className="relative h-1/6 border-8 border-green-500"></section>
+							<section className="relative h-1/6 border-8 border-blue-500"></section>
+						</div>
 					</main>
 				</div>
 			</div>
