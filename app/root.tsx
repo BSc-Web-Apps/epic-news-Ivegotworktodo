6ebc8137b87df3f04a1e3cfb084c3210ee16d49c
Joshua Blake <115797116+Ivegotworktodo@users.noreply.github.com>
2024-11-28 16:08:37 +0000

    Added Security elements

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,7 @@
 import { type LinksFunction } from '@remix-run/node'
 import { Outlet, useLoaderData } from '@remix-run/react'
+import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
+import { HoneypotProvider } from 'remix-utils/honeypot/react'
 import Document from '~/components/shared-layout/Document'
 import ThemeSwitch from '~/components/shared-layout/ThemeSwitch'
 import { useNonce } from '~/utils/nonce-provider.ts'
@@ -22,36 +24,40 @@ export default function App() {
 	const theme = useTheme()
 
 	return (
-		<Document nonce={nonce} theme={theme}>
-			<div className="flex h-screen flex-col justify-between">
-				<HeaderWithSearch />
-				<div className="flex-1">
-					<Outlet />
-				</div>
-				<div className="container flex justify-between pb-5">
-					<ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
-				</div>
-				<div className="flex-1">
-					<main className="container grid h-full place-items-center">
-						<h1 className="px-60 text-center text-mega text-red-400">
-							Welcome!
-						</h1>
-						<p className="text-5xl">This is a great website</p>
-						<button className="transition=all h-1/2 w-1/3 rounded bg-blue-500 px-4 py-2 font-bold duration-500 hover:bg-red-700 hover:text-6xl md:text-lg lg:text-xl">
-							Goodbye!
-						</button>
-						<div className="w-1/2">
-							<section className="relative h-1/6 border-8 border-yellow-500"></section>
-							<section className="relative h-1/6 border-8 border-green-500"></section>
-							<section className="relative h-1/6 border-8 border-amber-500"></section>
+		<AuthenticityTokenProvider token={data.csrfToken}>
+			<HoneypotProvider {...data.honeyProps}>
+				<Document nonce={nonce} theme={theme}>
+					<div className="flex h-screen flex-col justify-between">
+						<HeaderWithSearch />
+						<div className="flex-1">
+							<Outlet />
 						</div>
-					</main>
-				</div>
-				<FooterMenuRight
-					companyName="Jersey Morning News"
-					altText="Jersey Morning News Logo"
-				/>
-			</div>
-		</Document>
+						<div className="container flex justify-between pb-5">
+							<ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
+						</div>
+						<div className="flex-1">
+							<main className="container grid h-full place-items-center">
+								<h1 className="px-60 text-center text-mega text-red-400">
+									Welcome!
+								</h1>
+								<p className="text-5xl">This is a great website</p>
+								<button className="transition=all h-1/2 w-1/3 rounded bg-blue-500 px-4 py-2 font-bold duration-500 hover:bg-red-700 hover:text-6xl md:text-lg lg:text-xl">
+									Goodbye!
+								</button>
+								<div className="w-1/2">
+									<section className="relative h-1/6 border-8 border-yellow-500"></section>
+									<section className="relative h-1/6 border-8 border-green-500"></section>
+									<section className="relative h-1/6 border-8 border-amber-500"></section>
+								</div>
+							</main>
+						</div>
+						<FooterMenuRight
+							companyName="Jersey Morning News"
+							altText="Jersey Morning News Logo"
+						/>
+					</div>
+				</Document>
+			</HoneypotProvider>
+		</AuthenticityTokenProvider>
 	)
 }
