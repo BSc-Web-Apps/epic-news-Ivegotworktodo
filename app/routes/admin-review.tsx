f38d697ac2acba9613f0326477a9f89352fa91d4
Joshua Blake <115797116+Ivegotworktodo@users.noreply.github.com>
2024-12-12 14:11:44 +0000

    Added Admin role check for Admin Review page

--- app/routes/admin-review.tsx
+++ app/routes/admin-review.tsx
@@ -1,9 +1,13 @@
-import { cn } from '#app/utils/misc.tsx'
-import { json } from '@remix-run/node'
+import { type LoaderFunctionArgs, json } from '@remix-run/node'
 import { NavLink, Outlet, useLoaderData } from '@remix-run/react'
+import { cn } from '#app/utils/misc.tsx'
 import { prisma } from '~/utils/db.server.ts'
+import { requireUserWithRole } from '~/utils/permissions.server.js'
+import { Button } from '~/components/atoms/Button.js'
+import { GeneralErrorBoundary } from '~/components/ErrorBoundary.js'
 
-export async function loader() {
+export async function loader({ request }: LoaderFunctionArgs) {
+	await requireUserWithRole(request, 'admin')
 	const allArticles = await prisma.article.findMany({
 		select: { id: true, title: true, isPublished: true },
 	})
@@ -64,5 +68,25 @@ export default function ArticlesRoute() {
 				</div>
 			</div>
 		</main>
+	)
+}
+
+export function ErrorBoundary() {
+	return (
+		<GeneralErrorBoundary
+			statusHandlers={{
+				403: () => (
+					<div>
+						<p>You are not allowed to access this page.</p>
+						<p>
+							Please login with an administrator account, or contact support.
+						</p>
+						<Button>
+							<NavLink to="/login">Login</NavLink>
+						</Button>
+					</div>
+				),
+			}}
+		/>
 	)
 }
